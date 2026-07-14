0bc1fbfb05ffcedcf97dfbde3c0caa11c0fd42e6
Alexander Landman <alexlandman1998@gmail.com>
2026-07-14 18:48:14 +0200

    ag2g

--- the-it-guy/src/main.jsx
+++ the-it-guy/src/main.jsx
@@ -3,7 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const ROOT_INSTANCE_KEY = '__arch9ReactRoot'
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Arch9 could not start because the root element is missing.')
+}
+
+const root = rootElement[ROOT_INSTANCE_KEY] || createRoot(rootElement)
+rootElement[ROOT_INSTANCE_KEY] = root
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
